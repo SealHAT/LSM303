b57ee7d42e4dc144647b4ef236a3ac4a9c89eb2e
Huijie Pan <hpan5@crm.ucsc.edu>
2018-02-23 18:15:11 -0800

    addnew main

--- example/FeatherM0/main.c
+++ example/FeatherM0/main.c
@@ -1,25 +1,38 @@
 #include <atmel_start.h>
-#include "usb_start.h"
+ #include "LSM303.c"
+ 
+const uint8_t MASK = 0b00001000;
+const uint8_t IamLX= 0b01000001;
+const uint8_t IamMAG = 0b00111101;
+
+void clearREADYbit()
+{
+	acc_readReg1(&wire,ACC_OUT_X_L);
+	acc_readReg1(&wire,ACC_OUT_X_H);
+	acc_readReg1(&wire,ACC_OUT_Y_L);
+	acc_readReg1(&wire,ACC_OUT_Y_H);
+	acc_readReg1(&wire,ACC_OUT_Z_L);
+	acc_readReg1(&wire,ACC_OUT_Z_H);
+}
 
 int main(void)
 {
-	int letter;
-	
+	//gpio_set_pin_level(LED0, true);
+	uint8_t OUT = 0b00000000;
+	//uint8_t NameMAG = 0b00000000;
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
+	imu_init(&wire);
+	////////////////////Accelerometer self-test
+	acc_writeReg1(&wire,ACC_CTRL1, 0x2F); //Initialize sensor, turn on sensor
+	acc_writeReg1(&wire,ACC_CTRL4, 0x04); //FS = 2g
+	do{
+		clearREADYbit();
+		OUT = acc_readReg1(&wire,ACC_STATUS);
+		gpio_toggle_pin_level(LED0);
+		delay_ms(2000);
+	}while((OUT&MASK) != 0);
 	
-	while (1) {
-		/* Turn on LED if the DTR signal is set (serial terminal open on host) */
-		gpio_set_pin_level(LED_BUILTIN, usb_dtr());
-		
-		/* If the host has a terminal open then try to read, and if we read send it back */
-		if(usb_dtr()) {
-			letter = usb_get();
-			if(letter > 0) {
-				usb_put(letter);
-				usb_flush();
-			}
-		}
-
-	}
+	acc_writeReg1(&wire,ACC_CTRL5, 0x04); //Enable self-test
 }
+
