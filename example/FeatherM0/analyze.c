65962f354c062584565eb87d898d89724e5a9a9e
Huijie Pan <hpan5@ucsc.edu>
2018-03-07 15:21:56 -0800

    New files

---
+++ example/FeatherM0/analyze.c
@@ -0,0 +1,10 @@
+/*
+ * analyze.c
+ *
+ * Created: 3/7/2018 1:47:44 PM
+ *  Author: hpan5
+ */ 
+float pitch_est(AxesRaw_t Axes){
+	float pitch;
+	pitch = 
+}
---
+++ example/FeatherM0/analyze.h
@@ -0,0 +1,16 @@
+/*
+ * analyze.h
+ *
+ * Created: 3/7/2018 1:48:04 PM
+ *  Author: hpan5
+ */ 
+
+
+#ifndef ANALYZE_H_
+#define ANALYZE_H_
+
+
+
+
+
+#endif /* ANALYZE_H_ */
