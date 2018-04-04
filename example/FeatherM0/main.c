530d8f622d324359dc74b9d8b5f052ed9352b187
Ethan Slattery <theslat@gmail.com>
2018-04-04 16:02:32 -0700

    reverted main to master version

--- example/FeatherM0/main.c
+++ example/FeatherM0/main.c
@@ -10,7 +10,6 @@ int32_t printAxis(AxesSI_t* reading);
 
 int main(void)
 {
-	//char  output[STRING_SIZE];		/* A string used for output on USB */
 	AxesSI_t xcel;					/* Accelerometer reading */
 	AxesSI_t mag;					/* Magnetometer reading */
 	//int16_t   temp;					/* Magnetometer temperature */
@@ -21,7 +20,6 @@ int main(void)
 	lsm303_init(&wire);
 	lsm303_startAcc(ACC_FS_2G, ACC_ODR_50_Hz);
 	lsm303_startMag(MAG_MODE_CONTINUOUS, MAG_DO_40_Hz, MAG_TEMP_ENABLE);
-	
 	for(;;) {
 
 		/* Read and print the Accelerometer if it is ready */
@@ -30,7 +28,6 @@ int main(void)
             gpio_set_pin_level(LED_BUILTIN, true);
             xcel  = lsm303_getGravity();
             gpio_set_pin_level(LED_BUILTIN, false);
-			
 			/* Print the data if USB is available */
 			if(usb_dtr()) {
 				err = printAxis(&xcel);
@@ -47,10 +44,9 @@ int main(void)
 // 		gpio_set_pin_level(LED_BUILTIN, true);
 // 		mag  = lsm303_getGauss();
 // 		gpio_set_pin_level(LED_BUILTIN, false);
-// 		
 // 		/* Print the data if USB is available */
 // 		if(usb_dtr()) {
-// 			err = printAxis(&mag);	
+// 			err = printAxis(&mag);
 // 			if(err < 0) {
 // 				delay_ms(1);
 // 				usb_write("ERROR!\n", 7);
