49f0957ede7f10ce2c3b4478c220d85d87ee233b
Ethan Slattery <eslatter@ucsc.edu>
2018-05-30 14:21:11 -0700

    improvements to software motion detection

--- LSM303AGR.c
+++ LSM303AGR.c
@@ -72,6 +72,95 @@ static int32_t readContinous(const LSM303_DEV_ADDR_t SLAVE_ADDRESS, uint8_t STAR
 	return _i2c_m_sync_transfer(&lsm303c_sync.device, &msg);
 }
 
+static inline uint_fast8_t acc_getScaleConstant(void)
+{
+    // different Scales, there are 3 modes and 4 full scale settings. these are mg/LSB values from data sheet page 13. these are rounded ints from the STM API
+    static const uint8_t intScale[3][4] = {{1,   2,  4,  12},
+    {4,   8, 16,  48},
+    {16, 32, 64, 192}};
+    uint_fast8_t i, j;
+
+    switch(currentAccMode) {
+        case ACC_NORM_1_HZ:
+        case ACC_NORM_10_HZ:
+        case ACC_NORM_25_HZ:
+        case ACC_NORM_50_HZ:
+        case ACC_NORM_100_HZ:
+        case ACC_NORM_200_HZ:
+        case ACC_NORM_400_HZ:
+        case ACC_NORM_1344_HZ: i = 1;
+        break;
+        case ACC_LP_1_HZ:
+        case ACC_LP_10_HZ:
+        case ACC_LP_25_HZ:
+        case ACC_LP_50_HZ:
+        case ACC_LP_100_HZ:
+        case ACC_LP_200_HZ:
+        case ACC_LP_400_HZ:
+        case ACC_LP_1620_HZ:
+        case ACC_LP_5376_HZ: i = 2;
+        break;
+        case ACC_HR_1_HZ:
+        case ACC_HR_10_HZ:
+        case ACC_HR_25_HZ:
+        case ACC_HR_50_HZ:
+        case ACC_HR_100_HZ:
+        case ACC_HR_200_HZ:
+        case ACC_HR_400_HZ:
+        case ACC_HR_1344_HZ:
+        default :           i = 0;
+    };
+
+    switch(currentScale) {
+        case ACC_FS_4G:  j = 1;
+        break;
+        case ACC_FS_8G:  j = 2;
+        break;
+        case ACC_FS_16G: j = 3;
+        break;
+        case ACC_FS_2G:
+        default:         j = 0;
+    };
+
+    return intScale[i][j];
+}
+
+static inline uint_fast8_t acc_getShift(void)
+{
+    uint_fast8_t shift;
+    switch(currentAccMode) {
+        case ACC_NORM_1_HZ:
+        case ACC_NORM_10_HZ:
+        case ACC_NORM_25_HZ:
+        case ACC_NORM_50_HZ:
+        case ACC_NORM_100_HZ:
+        case ACC_NORM_200_HZ:
+        case ACC_NORM_400_HZ:
+        case ACC_NORM_1344_HZ: shift = 6;
+        break;
+        case ACC_LP_1_HZ:
+        case ACC_LP_10_HZ:
+        case ACC_LP_25_HZ:
+        case ACC_LP_50_HZ:
+        case ACC_LP_100_HZ:
+        case ACC_LP_200_HZ:
+        case ACC_LP_400_HZ:
+        case ACC_LP_1620_HZ:
+        case ACC_LP_5376_HZ: shift = 8;
+        break;
+        case ACC_HR_1_HZ:
+        case ACC_HR_10_HZ:
+        case ACC_HR_25_HZ:
+        case ACC_HR_50_HZ:
+        case ACC_HR_100_HZ:
+        case ACC_HR_200_HZ:
+        case ACC_HR_400_HZ:
+        case ACC_HR_1344_HZ:
+        default :            shift = 4;
+    };
+    return shift;
+}
+
 int32_t lsm303_init(struct i2c_m_sync_desc *const WIRE)
 {
   	lsm303c_sync  = *WIRE;
@@ -250,44 +339,53 @@ int32_t lsm303_acc_motionDetectRead(uint8_t* detect)
     return readReg(LSM303_ACCEL, ACC_INT1_SRC, detect);
 }
 
+void lsm303_acc_motionDetectSoft_init(MOTION_DETECT_t* filter, const int16_t threshold, const uint8_t sensitivity, const uint8_t scaler)
+{
+        filter->sensitivity = sensitivity;
+        filter->threshold   = threshold;
+        filter->duration    = 0;
+        filter->hp          = scaler;
+        filter->S.xAxis     = 0;
+        filter->S.yAxis     = 0;
+        filter->S.zAxis     = 0;
+}
+
 uint8_t lsm303_motionDetectSoft(MOTION_DETECT_t* filter, AxesRaw_t* buffer, const uint16_t LEN)
 {
-    uint_fast8_t i;          // LCV for the for loop
-    AxesRaw_t    highpass;   // highpass filtered data for detection
-    int16_t      negThreash; // negative threshold calculated once
-    uint8_t      detection;  // return value
-
-    negThreash = filter->threshold * -1;
+    uint_fast8_t i;                 // LCV for the for loop
+    AxesRaw_t    highpass;          // highpass filtered data for detection
+    uint8_t      detection = 0x00;  // return value
+    uint_fast8_t shift = acc_getShift();
 
     for(i=0; i < LEN; i++) {
-        // Perform exponential moving average low pass filtering. hard coded .5 alpha value.
-        // sensitivity. as Alpha gets smaller the cutoff gets lower
-        filter->S.xAxis = (buffer[i].xAxis >> filter->hp) + (filter->S.xAxis - (filter->S.xAxis >> filter->hp));
-        filter->S.yAxis = (buffer[i].yAxis >> filter->hp) + (filter->S.yAxis - (filter->S.yAxis >> filter->hp));
-        filter->S.zAxis = (buffer[i].zAxis >> filter->hp) + (filter->S.zAxis - (filter->S.zAxis >> filter->hp));
+        // Perform exponential moving average low pass filtering.
+        // as hp gets smaller the cutoff gets lower
+        filter->S.xAxis = ((buffer[i].xAxis >> shift) >> filter->hp) + (filter->S.xAxis - (filter->S.xAxis >> filter->hp));
+        filter->S.yAxis = ((buffer[i].yAxis >> shift) >> filter->hp) + (filter->S.yAxis - (filter->S.yAxis >> filter->hp));
+        filter->S.zAxis = ((buffer[i].zAxis >> shift) >> filter->hp) + (filter->S.zAxis - (filter->S.zAxis >> filter->hp));
 
         // get the highpass data by subtracting the low pass from the data
-        highpass.xAxis = buffer[i].xAxis - filter->S.xAxis;
-        highpass.yAxis = buffer[i].yAxis - filter->S.yAxis;
-        highpass.zAxis = buffer[i].zAxis - filter->S.zAxis;
+        highpass.xAxis = (buffer[i].xAxis >> shift) - filter->S.xAxis;
+        highpass.yAxis = (buffer[i].yAxis >> shift) - filter->S.yAxis;
+        highpass.zAxis = (buffer[i].zAxis >> shift) - filter->S.zAxis;
 
         // test for threshold detection
         if((filter->sensitivity & MOTION_INT_X_HIGH) && (highpass.xAxis >= filter->threshold)) {
             detection |= MOTION_INT_X_HIGH;
         }
-        if((filter->sensitivity & MOTION_INT_X_LOW) && (highpass.xAxis <= negThreash)) {
+        if((filter->sensitivity & MOTION_INT_X_LOW) && (highpass.xAxis <= (filter->threshold * -1))) {
             detection |= MOTION_INT_X_LOW;
         }
         if((filter->sensitivity & MOTION_INT_Y_HIGH) && (highpass.yAxis >= filter->threshold)) {
             detection |= MOTION_INT_Y_HIGH;
         }
-        if((filter->sensitivity & MOTION_INT_Y_LOW) && (highpass.yAxis <= negThreash)) {
+        if((filter->sensitivity & MOTION_INT_Y_LOW) && (highpass.yAxis <= (filter->threshold * -1))) {
             detection |= MOTION_INT_Y_LOW;
         }
         if((filter->sensitivity & MOTION_INT_Z_HIGH) && (highpass.zAxis >= filter->threshold)) {
             detection |= MOTION_INT_Z_HIGH;
         }
-        if((filter->sensitivity & MOTION_INT_Z_LOW) && (highpass.zAxis <= negThreash)) {
+        if((filter->sensitivity & MOTION_INT_Z_LOW) && (highpass.zAxis <= (filter->threshold * -1))) {
             detection |= MOTION_INT_Z_LOW;
         }
     }
@@ -480,95 +578,6 @@ int32_t lsm303_acc_FIFOread(AxesRaw_t* buf, const uint32_t LEN, bool* overrun)
 int32_t lsm303_mag_rawRead(AxesRaw_t* rawMag)
 {
     return readContinous(LSM303_MAG, MAG_OUTX_L, (uint8_t*)rawMag, 6);
-}
-
-static inline uint_fast8_t acc_getScaleConstant(void)
-{
-    // different Scales, there are 3 modes and 4 full scale settings. these are mg/LSB values from data sheet page 13. these are rounded ints from the STM API
-    static const uint8_t intScale[3][4] = {{1,   2,  4,  12},
-                                           {4,   8, 16,  48},
-                                           {16, 32, 64, 192}};
-    uint_fast8_t i, j;
-
-    switch(currentAccMode) {
-        case ACC_NORM_1_HZ:
-        case ACC_NORM_10_HZ:
-        case ACC_NORM_25_HZ:
-        case ACC_NORM_50_HZ:
-        case ACC_NORM_100_HZ:
-        case ACC_NORM_200_HZ:
-        case ACC_NORM_400_HZ:
-        case ACC_NORM_1344_HZ: i = 1;
-                               break;
-        case ACC_LP_1_HZ:
-        case ACC_LP_10_HZ:
-        case ACC_LP_25_HZ:
-        case ACC_LP_50_HZ:
-        case ACC_LP_100_HZ:
-        case ACC_LP_200_HZ:
-        case ACC_LP_400_HZ:
-        case ACC_LP_1620_HZ:
-        case ACC_LP_5376_HZ: i = 2;
-                             break;
-        case ACC_HR_1_HZ:
-        case ACC_HR_10_HZ:
-        case ACC_HR_25_HZ:
-        case ACC_HR_50_HZ:
-        case ACC_HR_100_HZ:
-        case ACC_HR_200_HZ:
-        case ACC_HR_400_HZ:
-        case ACC_HR_1344_HZ:
-        default :           i = 0;
-    };
-
-    switch(currentScale) {
-        case ACC_FS_4G:  j = 1;
-                         break;
-        case ACC_FS_8G:  j = 2;
-                         break;
-        case ACC_FS_16G: j = 3;
-                         break;
-        case ACC_FS_2G:
-        default:         j = 0;
-    };
-
-    return intScale[i][j];
-}
-
-static inline uint_fast8_t acc_getShift(void)
-{
-    uint_fast8_t shift;
-    switch(currentAccMode) {
-        case ACC_NORM_1_HZ:
-        case ACC_NORM_10_HZ:
-        case ACC_NORM_25_HZ:
-        case ACC_NORM_50_HZ:
-        case ACC_NORM_100_HZ:
-        case ACC_NORM_200_HZ:
-        case ACC_NORM_400_HZ:
-        case ACC_NORM_1344_HZ: shift = 6;
-                               break;
-        case ACC_LP_1_HZ:
-        case ACC_LP_10_HZ:
-        case ACC_LP_25_HZ:
-        case ACC_LP_50_HZ:
-        case ACC_LP_100_HZ:
-        case ACC_LP_200_HZ:
-        case ACC_LP_400_HZ:
-        case ACC_LP_1620_HZ:
-        case ACC_LP_5376_HZ: shift = 8;
-                             break;
-        case ACC_HR_1_HZ:
-        case ACC_HR_10_HZ:
-        case ACC_HR_25_HZ:
-        case ACC_HR_50_HZ:
-        case ACC_HR_100_HZ:
-        case ACC_HR_200_HZ:
-        case ACC_HR_400_HZ:
-        case ACC_HR_1344_HZ:
-        default :            shift = 4;
-    };
-    return shift;
 }
 
 int16_t acc_MgToRaw(int16_t milliG)
